66cf972c95e1814976a3590259e91e5e7d196a0e
Dariel f <111662959+dariel012@users.noreply.github.com>
2024-02-04 23:02:04 -0400

    Add files via upload
    Confeccionar una clase Persona que tenga como atributos el nombre y la edad (definir las propiedades para poder acceder a dichos atributos). Definir como responsabilidad un método para imprimir. Plantear una segunda clase Empleado que herede de la clase Persona. Añadir un atributo sueldo ( y su propiedad) y el método para imprimir su sueldo. Definir un objeto de la clase Persona y llamar a sus métodos y propiedades. También crear un objeto de la clase Empleado y llamar a sus métodos y propiedades.

---
+++ programa4/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Persona
+{
+   
+    private string nombre;
+    private int edad;
+
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public int Edad
+    {
+        get { return edad; }
+        set { edad = value; }
+    }
+
+   
+    public void Imprimir()
+    {
+        Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+    }
+}
+
+class Empleado : Persona
+{
+    
+    private double sueldo;
+
+    public double Sueldo
+    {
+        get { return sueldo; }
+        set { sueldo = value; }
+    }
+
+    
+    public void ImprimirSueldo()
+    {
+        Console.WriteLine($"Sueldo: {Sueldo}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Persona persona = new Persona();
+        persona.Nombre = "Juan Pérez";
+        persona.Edad = 30;
+
+        
+        persona.Imprimir();
+
+       
+        Empleado empleado = new Empleado();
+        empleado.Nombre = "María López";
+        empleado.Edad = 25;
+        empleado.Sueldo = 50000;
+
+       
+        empleado.Imprimir();
+        empleado.ImprimirSueldo();
+    }
+}
